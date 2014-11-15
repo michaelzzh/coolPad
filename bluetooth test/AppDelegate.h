8bd455edb0abe7962f05e9b976d497ba787c9d6f
Zehao Zhang <zehaozhang@wl-30-127.cine.claremont.edu>
2014-11-14 18:50:31 -0800

    Initial Commit

---
+++ bluetooth test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bluetooth test
+//
+//  Created by Zehao Zhang on 14-11-14.
+//  Copyright (c) 2014年 Zehao Zhang Xin Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bluetooth test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bluetooth test
+//
+//  Created by Zehao Zhang on 14-11-14.
+//  Copyright (c) 2014年 Zehao Zhang Xin Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
